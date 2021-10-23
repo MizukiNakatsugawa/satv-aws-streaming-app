a01ee6325d03824672bd962762cb6c7b3012bac2
中津川 瑞樹 <m-nakatsugawa@satv.co.jp>
2021-10-23 15:18:02 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,99 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-container>
+          <amplify-auth-container>
+            <amplify-authenticator username-alias="email">
+               <amplify-sign-in
+                  header-text="サインイン画面"
+                  slot="sign-in"
+                ></amplify-sign-in>
+                <amplify-sign-up
+                  header-text="アカウント新規登録"
+                  slot="sign-up"
+                  username-alias="email"
+                  :formFields="formFields"
+                ></amplify-sign-up>
+            </amplify-authenticator>
+          </amplify-auth-container>
+    </v-container>
   </v-app>
 </template>
 
 <script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components';
+// import { Auth } from '@aws-amplify/auth';
+// import { AmplifyEventBus } from 'aws-amplify-vue';
+import { Auth } from 'aws-amplify';
 
 export default {
-  name: 'App',
+  name: 'AuthStateApp',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    });
+  },
+  // async beforeCreate() {
+  //   try {
+  //     this.user = await Auth.currentAuthenticatedUser();
+  //     this.signedIn = true;
+  //   } catch (err) {
+  //     this.signedIn = false;
+  //   }
+  //   // 認証ステータスが変わった時に呼び出されるイベントを登録
+  //   AmplifyEventBus.$on('authState', async (info) => {
+  //     if (info === 'signedIn') {
+  //       this.signedIn = true;
+  //       this.user = await Auth.currentAuthenticatedUser();
+  //     } else {
+  //       this.signedIn = false;
+  //       this.user = undefined;
+  //     }
+  //   });
+  // },
+  data() {
+    return {
+      formFields: [
+        {
+          type: 'email',
+          label: 'メールアドレス',
+          placeholder: 'Custom email placeholder',
+          inputProps: { required: true, autocomplete: 'username' },
+        },
+        {
+          type: 'password',
+          label: 'パスワード',
+          placeholder: 'Custom password placeholder',
+          inputProps: { required: true, autocomplete: 'new-password' },
+        },
+        {
+          type: 'phone_number',
+          label: '電話番号',
+          placeholder: 'Custom phone placeholder',
+        },
+      ],
+    };
+  },
+  methods: {
+    async signUp(username, password, email) {
+      try {
+        const user = await Auth.signUp({
+          username,
+          password,
+          attributes: {
+            email,
+          },
+        });
 
-  data: () => ({
-    //
-  }),
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+  },
+  beforeDestroy() {
+    return onAuthUIStateChange;
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import Amplify from 'aws-amplify';
+// import Amplify, * as AmplifyModules from 'aws-amplify';
+// import { AmplifyPlugin } from 'aws-amplify-vue';
+import '@aws-amplify/ui-vue';
 import App from './App.vue';
+import awsExports from './aws-exports';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
+Amplify.configure(awsExports);
+// Vue.use(AmplifyPlugin, AmplifyModules);
 Vue.config.productionTip = false;
 
 new Vue({
